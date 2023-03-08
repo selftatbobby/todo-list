0b01277b87c3dab257fe177d9cc0f5f7c60b0250
Bobby Tat <messaroundacc1@hotmail.com>
2023-03-08 05:36:43 -0800

    Brainstorming how to allow users to create projects

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import "./style.css";
 
 
-//Factory function pattern
+//Factory function pattern to create todo items
 const todoFactory = (title, description, dueDate, priority) => {
     return {title, description, dueDate, priority}
 }
 let workout = todoFactory("workout", "go to the gym and do a session", "02/26/2023", "high");
-console.log(workout.dueDate);
+let carDelivery = todoFactory("pickup car", "pickup new car from dealership", "03/08/2023", "high")
 
 
 
-//Constructor pattern
+
+//Constructor pattern to create todo items
 const todoConstructor = function(title, description, dueDate, priority) {
     this.title = title;
     this.description = description;
@@ -18,17 +19,27 @@ const todoConstructor = function(title, description, dueDate, priority) {
     this.priority = priority;
 }
 let mealPrep = new todoConstructor("mealprep", "make food", "02/28/2023", "medium");
-console.log(mealPrep.dueDate);
 
-//Projects
+
+//Projects will be arrays
 let defaultProjects = [];
 let funProjects = [];
 defaultProjects.push(workout, mealPrep);
+defaultProjects.push(carDelivery);
 
 
 funProjects = defaultProjects.splice(0,1)
 console.log(defaultProjects[0].description);
 console.log(funProjects[0].description);
 
-defaultProjects["height"]=mealPrep.dueDate;
-console.log(defaultProjects["height"]);
+
+console.log(typeof(workout));
+console.log(defaultProjects);
+console.log(defaultProjects[1]);
+
+function createProject(projectName) {
+    let projectName = [];
+}
+
+createProject(camping);
+console.log(camping);
