62afbae224303c1c74ac1efac82228be2def9ddc
Bobby Tat <messaroundacc1@hotmail.com>
2023-03-14 05:41:59 -0700

    created a new module. Restructuring code in modules to align to project blueprint

--- src/index.js
+++ src/index.js
@@ -1,33 +1,26 @@
 import "./style.css";
+import { todoFactory, todoConstructor } from "./todo-factory.js";
+
+let defaultProjects = [];
+let funProjects = [];
 
 
-//Factory function pattern to create todo items
-const todoFactory = (title, description, dueDate, priority) => {
-    return {title, description, dueDate, priority}
-}
+
+
 let workout = todoFactory("workout", "go to the gym and do a session", "02/26/2023", "high");
 let carDelivery = todoFactory("pickup car", "pickup new car from dealership", "03/08/2023", "high")
 
+// let mealPrep = new todoConstructor("mealprep", "make food", "02/28/2023", "medium");
 
 
-
-//Constructor pattern to create todo items
-const todoConstructor = function(title, description, dueDate, priority, project) {
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
-}
-let mealPrep = new todoConstructor("mealprep", "make food", "02/28/2023", "medium");
 
 
 //Projects will be dom created elements, todo items will be appended to the dom elements/projects. Default project == inbox
 
 
 
-let defaultProjects = [];
-let funProjects = [];
-defaultProjects.push(workout, mealPrep);
+
+defaultProjects.push(workout);
 defaultProjects.push(carDelivery);
 
 
---
+++ src/todo-factory.js
@@ -0,0 +1,13 @@
+//Factory function pattern to create todo items
+const todoFactory = (title, description, dueDate, priority) => {
+    return {title, description, dueDate, priority}
+}
+
+//Constructor pattern to create todo items
+const todoConstructor = function(title, description, dueDate, priority, project) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+}
+export {todoFactory, todoConstructor}; 
